9cedf07d871162a45cb9106079729d979ceac420
IMperiumX <yusufadell.dev@gmail.com>
2024-11-15 21:26:33 +0200

    feat: implement task queue with error handling in redq module

--- redq/redq.py
+++ redq/redq.py
@@ -1,1 +1,32 @@
-"""Main module."""
+from collections import namedtuple
+from functools import wraps
+from queue import Queue
+from typing import Callable, Deque
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+function_notation = namedtuple("function_notation", ["function", "args", "kwargs"])
+
+taks_queue: Deque = Queue(maxsize=3)
+
+
+class EnqueueError(Exception):
+    """Raised when the queue is full"""
+
+
+def redq(func: Callable) -> Callable:
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            taks_queue.append(function_notation(func, args, kwargs))
+            logger.info(f"Task added to queue: {func.__name__}")
+            return taks_queue
+
+        except Exception:
+            msg = f"Error adding task to queue: {func.__name__}"
+            logger.error(msg)
+            raise EnqueueError(msg)
+
+    return wrapper
